5e08a827f673d0c3ce97b11fb58f9a43ddd65a09
scottdiddy <obiefunafidelis@gmail.com>
2023-08-11 22:23:22 +0100

    update: added new function

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/scottdiddy/greetings
 
 go 1.20
+
+require(
+ github.com/scottdiddy/greetings v1.3.0
+)
--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package greetings
 
 import "fmt"
 
-func Spanish()  {
+func Spanish() {
 	fmt.Println("Speaking Spanishh")
 }
-func French()  {
+func French() {
 	fmt.Println("Comment ca varrrrrrrrr - French")
 }
-func English()  {
+func English() {
 	fmt.Println("How are you - English external")
+}
+
+func Arabic() {
+	fmt.Println("How are you - Arabic external")
 }
