98bfc49d60942e3ef3cb44c5e79baca75fe2efd2
scottdiddy <141838693+scottdiddy@users.noreply.github.com>
2023-08-11 13:53:16 +0100

    Update main.go
    Removed unexported functions

--- main.go
+++ main.go
@@ -6,11 +6,7 @@ func Spanish()  {
 	fmt.Println("Speaking Spanishh")
 }
 func French()  {
-	english()
 	fmt.Println("Comment ca varrrrrrrrr - French")
-}
-func english()  {
-	fmt.Println("How are you - English")
 }
 func English()  {
 	fmt.Println("How are you - English external")
